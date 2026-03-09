a90459f386c9dc9429353cf8150c4e74d9435131
Shoaib Shahriar <shoaibshahriar29@gmail.com>
2026-03-10 02:04:47 +0600

    feat: add external api client abstraction

---
+++ src/Problem01.BulkExternalRequests.Api/Clients/ExternalUserClient.cs
@@ -0,0 +1,25 @@
+﻿using Problem01.BulkExternalRequests.Api.Models.Dtos;
+using System.Text.Json;
+
+namespace Problem01.BulkExternalRequests.Api.Clients
+{
+    public class ExternalUserClient : IExternalUserClient
+    {
+        private readonly HttpClient _httpClient;
+        public ExternalUserClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<ExternalUserDto> GetUserAsync(int userId)
+        {
+            var response = await _httpClient.GetAsync($"users/{userId}");
+            
+            if(!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            return await response.Content.ReadFromJsonAsync<ExternalUserDto>();
+        }
+    }
+}
---
+++ src/Problem01.BulkExternalRequests.Api/Clients/IExternalUserClient.cs
@@ -0,0 +1,9 @@
+﻿using Problem01.BulkExternalRequests.Api.Models.Dtos;
+
+namespace Problem01.BulkExternalRequests.Api.Clients
+{
+    public interface IExternalUserClient
+    {
+        Task<ExternalUserDto> GetUserAsync(int userId);
+    }
+}
