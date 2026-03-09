829668d248d094e4a498f2177162e066dc97b4d4
Shoaib Shahriar <shoaibshahriar29@gmail.com>
2026-03-10 02:05:03 +0600

    feat: implement bulk fetch service using Task.WhenAll

---
+++ src/Problem01.BulkExternalRequests.Api/Controllers/BulkFetchController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Problem01.BulkExternalRequests.Api.Models.Requests;
+using Problem01.BulkExternalRequests.Api.Models.Responses;
+using Problem01.BulkExternalRequests.Api.Services;
+
+namespace Problem01.BulkExternalRequests.Api.Controllers
+{
+    [Route("api/bulk")]
+    [ApiController]
+    public class BulkFetchController : ControllerBase
+    {
+        private readonly IBulkFetchService _bulkFetchService;
+
+        public BulkFetchController(IBulkFetchService bulkFetchService)
+        {
+            _bulkFetchService = bulkFetchService;
+        }
+
+        [HttpPost("users")]
+        public async Task<ActionResult<BulkFetchResponse>> FetchUsers(BulkFetchRequest request)
+        {
+            var users = await _bulkFetchService.FetchUsersAsync(request.UserIds);
+
+            return Ok(new BulkFetchResponse
+            {
+                Users = users
+            });
+        }
+    }
+}
---
+++ src/Problem01.BulkExternalRequests.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Problem01.BulkExternalRequests.Api.Clients;
+using Problem01.BulkExternalRequests.Api.Services;
+
+namespace Problem01.BulkExternalRequests.Api.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IBulkFetchService, BulkFetchService>();
+            services.AddHttpClient<IExternalUserClient, ExternalUserClient>(client =>
+            {
+                client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+            });
+            return services;
+        }
+    }
+}
